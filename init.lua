1afed71d1929e0ffec310dd1bf76522be0ee677e
dawnfire05 <dawnfire05@naver.com>
2025-04-12 17:17:16 +0900

    feat: map caps lock to esc + change to english for vim

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
+local FRemap = require('modules.foundation_remapping')
+local remapper = FRemap.new()
+
 hs.alert.show("Hello, Se-Kai")
 hs.hotkey.bind({'shift','cmd'},"r", hs.reload)
+remapper:remap('capslock','f13')
+remapper:register()
+
+require('modules.inputsource_aurora')
+require('modules.vim')
+
