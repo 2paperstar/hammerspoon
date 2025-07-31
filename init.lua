da1e17d12303bdf147b2c8a18ed73f0a5c3bdfa9
2paperstar <lee@paperst.ar>
2025-07-30 19:11:07 -0700

    chore: my settings

--- init.lua
+++ init.lua
@@ -2,11 +2,10 @@ local FRemap = require('modules.foundation_remapping')
 local remapper = FRemap.new()
 
 hs.alert.show("Hello, World")
-hs.hotkey.bind({'shift','cmd'},"r", hs.reload)
-remapper:remap('capslock','f13')
-remapper:remap('rightcmd', 'f19')
+-- hs.hotkey.bind({'shift','cmd'},"r", hs.reload)
+remapper:remap('capslock', 'f13')
+remapper:remap('rightcmd', 'f18')
 remapper:register()
 
 require('modules.inputsource_aurora')
 require('modules.vim')
-
